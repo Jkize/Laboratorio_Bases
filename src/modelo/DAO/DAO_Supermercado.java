6fa9371269d9d639c6f28b23810a7ffaad80736a
Labing <Labing@Labing-PC>
2018-08-24 19:52:53 -0500

    v 1.31

--- src/modelo/DAO/DAO_Supermercado.java
+++ src/modelo/DAO/DAO_Supermercado.java
@@ -15,44 +15,20 @@ import modelo.Supermercado;
  *
  * @author PC02
  */
-public class DAO_Supermercado {
-    
+public class DAO_Supermercado implements DAO<Supermercado> {
+
     private RandomAccessFile archivo;
     private Arbol_Archivo_IdString arbol;
-    
+
     public DAO_Supermercado() throws FileNotFoundException {
         archivo = new RandomAccessFile("supermercado", "rw");
         arbol = new Arbol_Archivo_IdString("supermercado");
     }
-    
-    /**
-     * Busca un sm en especifico.
-     * 
-     * @param id String
-     * @return Supermercado: Retorna un objeto tipo Supermercado al encontrarlo,
-     * en caso contrario retorna un exception.
-     * @throws IOException 
-     */
-    public Supermercado buscarSM(String id) throws IOException{
-        int pos = (int) arbol.getPosArchivo(id);
-        Supermercado sm = new Supermercado();
-        archivo.seek(pos);
-        sm.setIdSM(archivo.readUTF());
-        sm.setNombreSM(archivo.readUTF());
-        sm.setDireccionSM(archivo.readUTF());
-        return sm;
-    }
-    
-    /**
-     * Crea un Supermercado
-     * @param sup Supermercado
-     * @return boolean: True en caso de crearlo correctamente, en caso contrario
-     * retornará un false.
-     * @throws IOException 
-     */
-    public boolean crearSM(Supermercado sup) throws IOException{
+ 
+    @Override
+    public boolean crear(Supermercado sup) throws FileNotFoundException, IOException {
         archivo.seek(archivo.length());
-        if(arbol.añadir(sup.getIdSM(), (int)archivo.length())){
+        if (arbol.añadir(sup.getIdSM(), (int) archivo.length())) {
             archivo.writeUTF(sup.getIdSM());
             archivo.writeUTF(sup.getNombreSM());
             archivo.writeUTF(sup.getDireccionSM());
@@ -60,36 +36,42 @@ public class DAO_Supermercado {
         }
         return false;
     }
-    
-    /**
-     * Elimina un supermercado apartir de su ID.
-     * 
-     * @param id String
-     * @return boolean: Retorna un True en caso de eliminarlo exitosamente, en
-     * caso contrarío retornará un false.
-     * @throws IOException 
-     */
-    public boolean eliminarSM(String id) throws IOException{
-        if (arbol.eliminar(id) && archivo.length() != 0) {
+
+    @Override
+    public Supermercado buscar(Object id) throws FileNotFoundException, IOException {
+        int pos = (int) arbol.getPosArchivo((String) id);
+        if (pos != -1) {
+            Supermercado sm = new Supermercado();
+            archivo.seek(pos);
+            sm.setIdSM(archivo.readUTF());
+            sm.setNombreSM(archivo.readUTF());
+            sm.setDireccionSM(archivo.readUTF());
+            return sm;
+        }
+        return null;
+
+    }
+
+    @Override
+    public boolean actualizar(Supermercado sup) throws FileNotFoundException, IOException {
+
+        int pos = (int) arbol.getPosArchivo(sup.getIdSM());
+        if (pos != -1) {
+            archivo.writeUTF(sup.getIdSM());
+            archivo.writeUTF(sup.getNombreSM());
+            archivo.writeUTF(sup.getDireccionSM());
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public boolean eliminar(Object id) throws FileNotFoundException, IOException {
+        if (archivo.length() != 0 && arbol.eliminar((String) id)) {
             return true;
         }
         return false;
     }
-    
-    /**
-     * Actualiza un supermercado apartir de un objeto tipo Supermercado.
-     * 
-     * @param sup Supermercado
-     * @return boolean: True en caso de actualizarlo correctamente, false en
-     * caso contrarío.
-     * @throws IOException 
-     */
-    public boolean actualizarSM(Supermercado sup) throws IOException{
-        int pos = (int) arbol.getPosArchivo(sup.getIdSM());
-        archivo.writeUTF(sup.getIdSM());
-        archivo.writeUTF(sup.getNombreSM());
-        archivo.writeUTF(sup.getDireccionSM());
-        return true;
-    }
-    
+
 }
